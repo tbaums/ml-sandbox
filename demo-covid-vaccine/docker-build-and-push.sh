a7aab5ed4f738c8dc6203feed16c5555e7acad95
Michael Tanenbaum <michael.tanenbaum@arrikto.com>
2021-03-09 10:43:19 -0500

    update vaccine-demo docker script to use new tag scheme

--- demo-covid-vaccine/docker-build-and-push.sh
+++ demo-covid-vaccine/docker-build-and-push.sh
@@ -1,2 +1,9 @@
-docker build . -t tbaums/vaccine-demo:2021-03-04
-docker push tbaums/vaccine-demo:2021-03-04
+git_commit=$(git log -1 --pretty=%h)
+timestamp=$(date +%Y%m%d%H%M)  
+tag=$git_commit-$timestamp
+image=tbaums/vaccine-demo:$tag
+# Comment out `pbcopy` below if not on MacOS.
+echo $image | pbcopy
+docker build . -t $image
+docker push $image
+docker system prune -f
